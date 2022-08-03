402a3dfd7e4ec68abdaa4bb554d58af6a472fcd7
ram-221 <rameshmalli857.com>
2022-08-03 17:38:28 +0530

    method can return SAD pass TC

---
+++ MoodAnalyzer.java
@@ -0,0 +1,15 @@
+package com.bridgelab.junit;
+
+public class MoodAnalyzer {
+
+	public MoodAnalyzer() {
+	}
+
+	public String MoodAnalyzerr(String message) {
+		if (message.contains("Sad"))
+			return "SAD";
+		else
+			return "HAPPY";
+	}
+	
+}
---
+++ MoodAnalyzerTest.java
@@ -0,0 +1,15 @@
+package com.bridgelab.junit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+
+	  @Test
+	    public void given_SadMood_Should_Return_Sad() {
+	        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+	        String mood = moodAnalyzer.MoodAnalyzerr("I am In a Sad Mood");
+	        Assert.assertEquals("SAD", mood);
+
+}
+}
