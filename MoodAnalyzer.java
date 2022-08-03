2225590e5934e9d7caeacb26b175be96751dce99
ram-221 <rameshmalli857.com>
2022-08-03 18:36:06 +0530

    using Constructer Return Sad

--- MoodAnalyzer.java
+++ MoodAnalyzer.java
@@ -1,15 +1,26 @@
-package com.bridgelab.junit;
+package com.repeattc;
+
+/**
+ * Purpose - “I am in Happy Mood” message Should Return Happy
+ *
+ */
 
 public class MoodAnalyzer {
+	private String message;
 
+	// Default Constructor
 	public MoodAnalyzer() {
 	}
 
-	public String MoodAnalyzerr(String message) {
-		if (message.contains("Sad"))
+	// Parameterized constructor
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
+
+	public String analyseMood() {
+		if (this.message.contains("Sad"))
 			return "SAD";
 		else
 			return "HAPPY";
 	}
-	
 }
--- MoodAnalyzerTest.java
+++ MoodAnalyzerTest.java
@@ -1,21 +1,16 @@
-package com.bridgelab.junit;
+package com.repeattc;
 
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.bridgelab.junit.MoodAnalyzer;
+
 public class MoodAnalyzerTest {
 
-	  @Test
-	    public void given_SadMood_Should_Return_Sad() {
-	        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-	        String mood = moodAnalyzer.analyzerMood("I am In a Sad Mood");
-	        Assert.assertEquals("SAD", mood);
- }
-	  
-	  @Test
-		public void given_AnyMood_Should_Return_HAPPY() {
-			MoodAnalyzer moodAnalyser = new MoodAnalyzer();
-			String mood = moodAnalyser.analyzerMood("I am in Any Mood");
-			Assert.assertEquals("HAPPY", mood);
+	@Test
+    public void given_SadMood_Should_Return_Sad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyzerMood("I am In a Sad Mood");
+        Assert.assertEquals("SAD", mood);
 }
 }
